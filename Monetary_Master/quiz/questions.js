bed625e976057c527c26608771091a25012e532a
AryanC23 <110075130+AryanC23@users.noreply.github.com>
2023-11-01 17:51:58 -0600

    Add files via upload

---
+++ Monetary_Master/quiz/questions.js
@@ -0,0 +1,61 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What is the purpose of a budget?",
+  answer: "To track and plan income and expenses",
+  options: [
+    "To restrict spending",
+    "To track and plan income and expenses",
+    "To maximize credit card usage",
+    "To eliminate all discretionary spending"
+
+  ]
+},
+  {
+  numb: 2,
+  question: "How can you distinguish between needs and wants when deciding what to save for?",
+  answer: "Evaluate and prioritize your needs before your wants",
+  options: [
+    "Prioritize everything you want equally",
+    "Only save for needs and ignore your wants",
+    "Evaluate and prioritize your needs before your wants",
+    "Save for wants first and then consider needs"
+
+  ]
+},
+  {
+  numb: 3,
+  question: "What is the primary purpose of a checking account for a teenager?",
+  answer: "To have easy access to their money for everyday expenses",
+  options: [
+    "To earn interest on their savings",
+    "To safely store their cash at home",
+    "To have easy access to their money for everyday expenses",
+    "To invest in the stock market"
+  ]
+},
+  {
+  numb: 4,
+  question: "Why might a teenager consider investing their money in stocks or other assets?",
+  answer: "To potentially grow their wealth over time through wise financial decisions",
+  options: [
+    "To get rich quickly with minimal efforte",
+    "To learn about financial markets from books and videos",
+    "To impress their friends with stock market knowledge",
+    "To potentially grow their wealth over time through wise financial decisions"
+  ]
+},
+  {
+  numb: 5,
+  question: "Why is it important for a teenager to set specific financial goals?",
+  answer: "To provide a clear direction for their money management and savings efforts",
+  options: [
+    "To impress their parents with their ambition",
+    "To have something to post on social media",
+    "To keep up with the latest fashion trends",
+    "To provide a clear direction for their money management and savings efforts"
+  ]
+},
+
+];
